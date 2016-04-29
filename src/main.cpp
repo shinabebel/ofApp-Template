75cec1eb79177e7b20e79217886357c6615738ae
hoba <hoba@ultracombos.com>
2016-04-29 10:54:08 +0800

    use ofGLFWWindowSettings for multi windows fullscreen

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,21 @@
 //========================================================================
 int main( ){
 #if 1
-	ofGLWindowSettings settings;
+	ofGLFWWindowSettings settings;
+	settings.multiMonitorFullScreen = true;
+	//ofGLWindowSettings settings;
 	settings.setGLVersion(4, 3);
 	settings.windowMode = OF_WINDOW;
-	ofCreateWindow(settings);
+	shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(settings);
+	shared_ptr<ofApp> mainApp(new ofApp);
+
+	ofRunApp(mainWindow, mainApp);
+	ofRunMainLoop();
 #else
 	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-#endif
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp(new ofApp());
-
+#endif
 }
